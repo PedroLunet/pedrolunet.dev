ba4fdfdcc3b23b452386c85d5405f35193408e6e
Pedro Lunet <pedrolunet@significa.co>
2025-07-30 20:40:41 +0100

    added font and updated header

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -5,9 +5,11 @@
 <header
 	class="flex h-[var(--navbar-height)] items-center justify-between bg-transparent px-4 md:px-6 lg:px-8"
 >
-	<div class="flex items-center gap-0.5">
+	<div class="flex items-center">
 		<h1 class="text-xl font-semibold text-muted dark:text-muted-dark">[</h1>
-		<h1 class="text-xl font-semibold text-accent dark:text-accent-dark">Pedro Lunet</h1>
+		<h1 class="text-xl font-semibold tracking-tighter text-accent dark:text-accent-dark">
+			Pedro Lunet
+		</h1>
 		<h1 class="text-xl font-semibold text-muted dark:text-muted-dark">]</h1>
 	</div>
 	<a href="/" class="text-text dark:text-text-dark" aria-label="Home">

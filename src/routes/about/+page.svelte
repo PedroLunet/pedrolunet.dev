34e2d67e0c52d594246322b91c37ea0a97c8a6d7
Pedro Lunet <pedrolunet@significa.co>
2025-07-30 14:18:34 +0100

    updated about me page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -10,7 +10,7 @@
 	<div class="flex h-fit flex-col items-start justify-center">
 		<SplitText
 			text="Pedro Lunet"
-			className=" text-3xl font-bold md:text-4xl lg:text-5xl leading-tight text-accent dark:text-accent-dark"
+			className=" text-3xl font-bold md:text-5xl leading-tight text-accent dark:text-accent-dark"
 			splitType="words"
 			delay={60}
 			duration={0.8}
@@ -21,7 +21,7 @@
 		/>
 		<SplitText
 			text="Web Developer"
-			className="text-2xl font-bold md:text-3xl lg:text-4xl leading-tight text-text dark:text-text-dark"
+			className="text-2xl font-bold **:md:text-4xl leading-tight text-text dark:text-text-dark"
 			splitType="words"
 			delay={60}
 			duration={0.8}
@@ -35,4 +35,8 @@
 			<Shiny text="Porto, Portugal" className="text-lg md:text-xl" speed={5} />
 		</div>
 	</div>
+	<p class="mb-6 w-full max-w-[400px] text-lg text-muted md:text-xl dark:text-muted-dark">
+		I like to design clean, responsive interfaces that feel quiet, thoughtful, and effortless. I
+		care about clarity, simplicity, and the small details that make things feel right.
+	</p>
 </section>

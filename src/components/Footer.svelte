3523f4b2327c8093769da08234c8f5812600c1b1
Pedro Lunet <pedrolunet@significa.co>
2025-08-01 16:32:46 +0100

    added footer

---
+++ src/components/Footer.svelte
@@ -0,0 +1,52 @@
+<script>
+	import { InstagramLogo, LinkedinLogo, GithubLogo, EnvelopeSimple } from 'phosphor-svelte';
+</script>
+
+<div
+	class="z-50 flex h-fit w-full flex-col items-center justify-center gap-4 p-4 font-semibold md:flex-row"
+>
+	<p class="text-center text-sm text-muted dark:text-muted-dark">Pedro Lunet</p>
+
+	<svg
+		width="6"
+		height="6"
+		viewBox="0 0 6 6"
+		fill="currentColor"
+		aria-hidden="true"
+		class="mx-2 text-muted dark:text-muted-dark"><circle cx="3" cy="3" r="3" /></svg
+	>
+	<div class="flex items-center gap-3">
+		<a
+			href="https://www.instagram.com/pedrolunet/"
+			tabindex="-1"
+			aria-label="Instagram"
+			class="cursor-pointer hover:opacity-80"
+		>
+			<InstagramLogo size={24} class="text-muted dark:text-muted-dark" />
+		</a>
+		<a
+			href="https://www.linkedin.com/in/pedrolunet/"
+			tabindex="-1"
+			aria-label="LinkedIn"
+			class="cursor-pointer hover:opacity-80"
+		>
+			<LinkedinLogo size={24} class="text-muted dark:text-muted-dark" />
+		</a>
+		<a
+			href="https://github.com/pedrolunet"
+			tabindex="-1"
+			aria-label="GitHub"
+			class="cursor-pointer hover:opacity-80"
+		>
+			<GithubLogo size={24} class="text-muted dark:text-muted-dark" />
+		</a>
+		<a
+			href="mailto:pedrolunet@gmail.com"
+			tabindex="-1"
+			aria-label="Email"
+			class="cursor-pointer hover:opacity-80"
+		>
+			<EnvelopeSimple size={24} class="text-muted dark:text-muted-dark" />
+		</a>
+	</div>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
 	import '../app.css';
+	import Footer from '../components/Footer.svelte';
 	import Header from '../components/Header.svelte';
 
 	let { children } = $props();
 </script>
 
-<Header />
-<main class="bg-background p-4 md:p-6 lg:p-8 dark:bg-background-dark">
-	{@render children()}
-</main>
+<div class="flex min-h-screen flex-col">
+	<Header />
+	<main class="flex-1 bg-background p-4 md:p-6 lg:p-8 dark:bg-background-dark">
+		{@render children()}
+	</main>
+	<Footer />
+</div>

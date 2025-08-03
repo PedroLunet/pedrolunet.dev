d4c4f6a5cbadf057e66ff8ba31656b3f66182ce7
Pedro Lunet <pedrolunet@significa.co>
2025-08-03 13:58:19 +0100

    installed shadcn svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,13 +2,33 @@
 	import '../app.css';
 	import Footer from '../components/Footer.svelte';
 	import Header from '../components/Header.svelte';
+	import { onMount } from 'svelte';
 
 	let { children } = $props();
+
+	onMount(() => {
+		const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+		const root = document.documentElement;
+		if (prefersDark) {
+			root.classList.add('dark');
+		} else {
+			root.classList.remove('dark');
+		}
+
+		// Listen for changes in system theme
+		window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (e) => {
+			if (e.matches) {
+				root.classList.add('dark');
+			} else {
+				root.classList.remove('dark');
+			}
+		});
+	});
 </script>
 
 <div class="flex min-h-screen flex-col">
 	<Header />
-	<main class="flex-1 bg-background p-4 md:p-6 lg:p-8 dark:bg-background-dark">
+	<main class="dark:bg-background-dark flex-1 bg-background p-4 md:p-6 lg:p-8">
 		{@render children()}
 	</main>
 	<Footer />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <section
-	class="flex min-h-[calc(70vh-var(--navbar-height))]	 flex-col bg-background px-4 py-10 text-text md:flex-row md:items-start md:justify-around md:gap-x-8 dark:bg-background-dark dark:text-text-dark"
+	class="text-text dark:bg-background-dark dark:text-text-dark flex min-h-[calc(70vh-var(--navbar-height))] flex-col bg-background px-4 py-10 md:flex-row md:items-start md:justify-around md:gap-x-8"
 >
 	<div>
 		<div class="flex h-fit flex-col items-start justify-center">
@@ -31,13 +31,13 @@
 				to={{ opacity: 1, y: 0 }}
 				threshold={0.1}
 			/>
-			<div class="mt-4 mb-4 flex items-center gap-1 text-text dark:text-text-dark">
-				<MapPin size={18} weight="bold" class="text-accent dark:text-accent-dark" />
+			<div class="text-text dark:text-text-dark mt-4 mb-4 flex items-center gap-1">
+				<MapPin size={18} weight="bold" class="dark:text-accent-dark text-accent" />
 				<Shiny text="Porto, Portugal" className="text-lg md:text-xl" speed={5} />
 			</div>
 		</div>
 		<p
-			class="mb-6 w-full max-w-[300px] text-base text-muted md:max-w-[350px] md:text-xl lg:max-w-[400px] dark:text-muted-dark"
+			class="dark:text-muted-dark mb-6 w-full max-w-[300px] text-base text-muted md:max-w-[350px] md:text-xl lg:max-w-[400px]"
 		>
 			I like to design clean, responsive interfaces that feel thoughtful and effortless with a focus
 			on clarity and the small details that matter.
@@ -46,44 +46,44 @@
 	<div>
 		<div class="mt-5 flex flex-col items-end justify-center">
 			<h3
-				class="w-fit border-b border-muted text-xl font-bold text-accent dark:border-muted-dark dark:text-accent-dark"
+				class="dark:border-muted-dark dark:text-accent-dark w-fit border-b border-muted text-xl font-bold text-accent"
 			>
 				About Me
 			</h3>
 			<p
-				class="mt-4 max-w-[300px] text-end text-base text-text md:max-w-[350px] lg:max-w-[400px] dark:text-text-dark"
+				class="text-text dark:text-text-dark mt-4 max-w-[300px] text-end text-base md:max-w-[350px] lg:max-w-[400px]"
 			>
 				I'm a frontend designer and developer from Porto, currently studying Informatics and
 				Computer Engineering at <a
 					href="https://www.fe.up.pt/"
-					class="font-semibold text-accent transition hover:text-[#8c2d19] dark:text-accent-dark"
+					class="dark:text-accent-dark font-semibold text-accent transition hover:text-[#8c2d19]"
 					>FEUP</a
 				>. I’m always learning and enjoy exploring how to make digital experiences feel clear and
 				intentional.
 			</p>
 			<a href="/about" class="group relative mt-2 inline-flex items-center gap-2 overflow-hidden">
-				<span class="relative z-10 font-extralight text-text dark:text-text-dark"
+				<span class="text-text dark:text-text-dark relative z-10 font-extralight"
 					>Get to know me!</span
 				>
 				<ArrowRight
 					size={20}
 					weight="bold"
-					class="relative z-10 text-accent transition-transform duration-300 group-hover:-translate-x-1 dark:text-accent-dark"
+					class="dark:text-accent-dark relative z-10 text-accent transition-transform duration-300 group-hover:-translate-x-1"
 				/>
 				<span
-					class="absolute bottom-0 left-0 h-[1px] origin-left scale-x-0 bg-muted transition-transform duration-300 group-hover:scale-x-100 dark:bg-muted-dark"
+					class="dark:bg-muted-dark absolute bottom-0 left-0 h-[1px] origin-left scale-x-0 bg-muted transition-transform duration-300 group-hover:scale-x-100"
 					style="width:calc(100% - 28px)"
 				></span>
 			</a>
 		</div>
 		<div class="mt-5 flex flex-col justify-center md:items-end">
 			<h3
-				class="w-fit border-b border-muted text-xl font-bold text-accent dark:border-muted-dark dark:text-accent-dark"
+				class="dark:border-muted-dark dark:text-accent-dark w-fit border-b border-muted text-xl font-bold text-accent"
 			>
 				My Work
 			</h3>
 			<p
-				class="mt-4 max-w-[300px] text-base text-text md:max-w-[350px] md:text-end lg:max-w-[400px] dark:text-text-dark"
+				class="text-text dark:text-text-dark mt-4 max-w-[300px] text-base md:max-w-[350px] md:text-end lg:max-w-[400px]"
 			>
 				You’ll find most of my work on GitHub. It's mostly university projects, both solo and in
 				groups. It’s where I’ve been learning by doing, experimenting, and trying to get things just
@@ -94,20 +94,19 @@
 				class="group relative mt-2 inline-flex items-center gap-2 overflow-hidden"
 			>
 				<span class="relative">
-					<span class="relative z-10 font-extralight text-text dark:text-text-dark"
+					<span class="text-text dark:text-text-dark relative z-10 font-extralight"
 						>See my projects!</span
 					>
 					<span
-						class="absolute right-0 bottom-0 left-0 h-[1px] origin-left scale-x-0 bg-muted transition-transform duration-300 group-hover:scale-x-100 dark:bg-muted-dark"
+						class="dark:bg-muted-dark absolute right-0 bottom-0 left-0 h-[1px] origin-left scale-x-0 bg-muted transition-transform duration-300 group-hover:scale-x-100"
 					></span>
 				</span>
 				<ArrowRight
 					size={20}
 					weight="bold"
-					class="relative z-10 text-accent transition-transform duration-300 group-hover:-translate-x-1 dark:text-accent-dark"
+					class="dark:text-accent-dark relative z-10 text-accent transition-transform duration-300 group-hover:-translate-x-1"
 				/>
 			</a>
 		</div>
 	</div>
 </section>
-

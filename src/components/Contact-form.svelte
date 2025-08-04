f3d1433bd31faa84ac56d8b4591c61e37d6d94bc
Pedro Lunet <pedrolunet@significa.co>
2025-08-04 17:05:18 +0100

    added form

--- src/components/Contact-form.svelte
+++ src/components/Contact-form.svelte
@@ -1,15 +1,22 @@
 <script lang="ts">
 	import * as Drawer from '$lib/components/ui/drawer/index.js';
 	import { Button } from '$lib/components/ui/button/index.js';
+	import { Input } from '$lib/components/ui/input/index.js';
+	import { Textarea } from '$lib/components/ui/textarea/index.js';
 </script>
 
 <Drawer.Root>
 	<Drawer.Trigger><Button variant="outline" size="lg">Contact Me</Button></Drawer.Trigger>
 	<Drawer.Content>
 		<Drawer.Header>
-			<Drawer.Title>Are you sure absolutely sure?</Drawer.Title>
-			<Drawer.Description>This action cannot be undone.</Drawer.Description>
+			<Drawer.Title>Contact Me</Drawer.Title>
+			<Drawer.Description>I'd love to hear from you!</Drawer.Description>
 		</Drawer.Header>
+		<form class="flex flex-col gap-4 px-4">
+			<Input type="text" placeholder="Your Name" required class="w-full" />
+			<Input type="email" placeholder="Your Email" required class="w-full" />
+			<Textarea placeholder="Your Message" required class="h-32 w-full" />
+		</form>
 		<Drawer.Footer>
 			<Button>Submit</Button>
 			<Drawer.Close>Cancel</Drawer.Close>

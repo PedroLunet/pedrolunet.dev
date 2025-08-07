9068255c9a2ce5d8cc3f045250d1178462cbde40
Pedro Lunet <pedrolunet@significa.co>
2025-08-07 16:49:24 +0100

    fixed techbadge

--- src/components/TechBadge.svelte
+++ src/components/TechBadge.svelte
@@ -7,7 +7,7 @@
 
 <Badge
 	variant="outline"
-	class="flex items-center gap-2 px-3 py-2 text-base"
+	class="flex items-center gap-2 px-3 py-2 text-base border-1"
 	style="border-color: {technology.color}40"
 >
 	<img src={technology.icon} alt={technology.name} class="h-6 w-6 object-contain" loading="lazy" />

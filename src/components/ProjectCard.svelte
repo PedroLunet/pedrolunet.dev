8fb90ac8151eb61788eae276019977aa0e2766ec
Pedro Lunet <pedrolunet@significa.co>
2025-08-07 12:05:51 +0100

    updated project cards styling

--- src/components/ProjectCard.svelte
+++ src/components/ProjectCard.svelte
@@ -9,6 +9,9 @@
 	import { Badge } from '$lib/components/ui/badge/index.js';
 	import { Button } from '$lib/components/ui/button/index.js';
 	import type { Project } from '$lib/data/projects.js';
+	import GithubIcon from '@lucide/svelte/icons/github';
+	import GlobeIcon from '@lucide/svelte/icons/globe';
+	import PlayIcon from '@lucide/svelte/icons/play';
 
 	export let project: Project;
 
@@ -19,7 +22,7 @@
 <Card class="relative flex h-full flex-col transition-shadow duration-200 hover:shadow-lg">
 	<!-- Main Technology Icon in top-left corner -->
 	<div class="absolute top-4 left-4 z-10">
-		<img src={mainTech.icon} alt={mainTech.name} class="h-8 w-8" title={mainTech.name} />
+		<img src={mainTech.icon} alt={mainTech.name} class="h-12 w-12" title={mainTech.name} />
 	</div>
 
 	<CardHeader class="pt-16">
@@ -32,19 +35,19 @@
 		<div class="mt-auto flex gap-2">
 			{#if project.links.github}
 				<Button variant="outline" size="sm" href={project.links.github} target="_blank">
-					<span class="mr-1">🐙</span>
+					<GithubIcon class="mr-1 h-4 w-4" />
 					GitHub
 				</Button>
 			{/if}
 			{#if project.links.website}
 				<Button variant="default" size="sm" href={project.links.website} target="_blank">
-					<span class="mr-1">🌐</span>
+					<GlobeIcon class="mr-1 h-4 w-4" />
 					Website
 				</Button>
 			{/if}
 			{#if project.links.demo}
 				<Button variant="secondary" size="sm" href={project.links.demo} target="_blank">
-					<span class="mr-1">🎬</span>
+					<PlayIcon class="mr-1 h-4 w-4" />
 					Demo
 				</Button>
 			{/if}

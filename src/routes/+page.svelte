33488a1faa0547cc54d5907fcd360befdb19d429
Pedro Lunet <pedrolunet@gmail.com>
2025-11-17 18:51:53 +0000

    add theme switch for testing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <script>
+	import ThemeSwitch from '../components/ThemeSwitch.svelte';
 </script>
 
 <div class="flex min-h-screen items-center justify-center">
+	<ThemeSwitch />
 	<h1 class="text-8xl font-extrabold text-primary">Pedro Lunet.</h1>
 </div>

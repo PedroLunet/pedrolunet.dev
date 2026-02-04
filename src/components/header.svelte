19f05e26147de92fb21077eff6a98158fe126577
Pedro Lunet <pedrolunet@gmail.com>
2026-02-04 15:02:12 +0000

    updated header layout

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -6,7 +6,7 @@
 	class="flex h-12 flex-row items-center justify-between px-6 text-base font-semibold tracking-tight md:h-18 md:px-9 md:text-xl"
 >
 	<span>pedro lunet</span>
-	<div>
-		<span>portfolio </span><TumblerYear year="2026" />
+	<div class="flex flex-row items-center">
+		<span>portfolio </span><TumblerYear />
 	</div>
 </header>

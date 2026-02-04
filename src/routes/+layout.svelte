7bec35e23c5f3165c7bce336885846aaaab6cc51
Pedro Lunet <pedrolunet@gmail.com>
2026-02-04 15:02:27 +0000

    added header to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,12 @@
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
 
+	import Header from '$lib/../components/header.svelte';
+
 	let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
+
+<Header />
 {@render children()}

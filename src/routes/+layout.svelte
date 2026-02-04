c5adbbcc598f4e14e6c2c16554aa76374c55f980
Pedro Lunet <pedrolunet@gmail.com>
2026-02-04 15:57:27 +0000

    updated layout and initialized lenis for smooth scroll and animation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,37 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
+	import Lenis from '@studio-freight/lenis';
+
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
-
-	import Header from '$lib/../components/header.svelte';
+	import Header from '$lib/components/header.svelte';
 
 	let { children } = $props();
+
+	onMount(() => {
+		const lenis = new Lenis({
+			duration: 1.2,
+			easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+			orientation: 'vertical',
+			smoothWheel: true
+		});
+
+		function raf(time: number) {
+			lenis.raf(time);
+			requestAnimationFrame(raf);
+		}
+
+		requestAnimationFrame(raf);
+	});
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	<link rel="preload" href="/fonts/Switzer-Variable.ttf" as="font" type="font/ttf" crossorigin />
+</svelte:head>
 
 <Header />
-{@render children()}
+
+<main class="w-full">
+	{@render children()}
+</main>

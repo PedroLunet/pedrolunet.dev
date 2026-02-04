eb460c0abf5c9f1cac3d08661415e7ab6ca57fab
Pedro Lunet <pedrolunet@gmail.com>
2026-02-04 17:35:29 +0000

    updated header height using new variables

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <header
-	class="flex h-12 flex-row items-center justify-between px-6 text-base font-semibold tracking-tight md:h-18 md:px-9 md:text-xl"
+	class="flex h-(--header-height-mobile) flex-row items-center justify-between px-6 text-base font-semibold tracking-tight md:h-(--header-height-desktop) md:px-9 md:text-xl"
 >
 	<a href="/"> pedro lunet </a>
 	<div class="flex flex-row items-center">

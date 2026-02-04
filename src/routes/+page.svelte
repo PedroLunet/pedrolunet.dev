85edf5038d3534e5df12df09214f44044017b93b
Pedro Lunet <pedrolunet@gmail.com>
2026-02-04 17:10:49 +0000

    adjusted sizes and layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,13 @@
-<div class="items-left flex h-full flex-col justify-center">
-	<div class="flex flex-row">
-		<h1>Pedro</h1>
-		<div class="h-10 w-20 bg-accent"></div>
-		<h1>Lunet</h1>
+<div class="flex h-svh w-full flex-col justify-center px-6 md:px-9">
+	<div class="flex flex-wrap items-center gap-4 md:gap-8">
+		<h1 class="text-6xl leading-none font-medium tracking-tighter md:text-9xl">Pedro</h1>
+
+		<div class="h-12 w-24 bg-accent md:h-24 md:w-48"></div>
+
+		<h1 class="text-6xl leading-none font-medium tracking-tighter md:text-9xl">Lunet</h1>
 	</div>
-	<h2>frontend dev</h2>
-	<h3>porto, portugal</h3>
+
+	<h2 class="text-2xl font-medium tracking-tight text-text-secondary md:text-7xl">frontend dev</h2>
+
+	<h3 class="mt-2 text-lg font-normal text-accent md:text-2xl">porto, portugal</h3>
 </div>

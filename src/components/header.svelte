f9aeae8a717d42892044fec4efdf11e308f4c4b0
Pedro Lunet <pedrolunet@gmail.com>
2026-02-04 14:56:59 +0000

    created header component

---
+++ src/components/header.svelte
@@ -0,0 +1,12 @@
+<script>
+	import TumblerYear from './tumblerYear.svelte';
+</script>
+
+<header
+	class="flex h-12 flex-row items-center justify-between px-6 text-base font-semibold tracking-tight md:h-18 md:px-9 md:text-xl"
+>
+	<span>pedro lunet</span>
+	<div>
+		<span>portfolio </span><span class="text-text-secondary">2026</span>
+	</div>
+</header>

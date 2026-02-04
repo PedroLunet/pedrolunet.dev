fb17fa1bf11e68a2e4dbf16b4eea5a5c1f61816a
Pedro Lunet <pedrolunet@gmail.com>
2026-02-04 22:24:54 +0000

    updated animations to prevent flashes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,24 +6,27 @@
 		let mm = gsap.matchMedia();
 		const tl = gsap.timeline();
 
-		tl.addLabel('start').from(
+		gsap.set('h1, h2, h3', { y: 20, autoAlpha: 0 });
+
+		tl.addLabel('start').to(
 			'h1, h2, h3',
 			{
-				y: 20,
-				opacity: 0,
+				y: 0,
+				autoAlpha: 1,
 				duration: 1.2,
 				ease: 'power3.out',
-				stagger: 0.5
+				stagger: 0.3
 			},
 			'start'
 		);
 
 		mm.add('(min-width: 768px)', () => {
-			tl.from(
+			tl.fromTo(
 				'.bg-accent',
+				{ width: 0, autoAlpha: 1 },
 				{
-					width: 0,
-					duration: 2,
+					width: '12rem',
+					duration: 2.5,
 					ease: 'power4.out'
 				},
 				'-=1.0'
@@ -31,11 +34,12 @@
 		});
 
 		mm.add('(max-width: 767px)', () => {
-			tl.from(
+			tl.fromTo(
 				'.bg-accent',
+				{ height: 0, autoAlpha: 1 },
 				{
-					height: 0,
-					duration: 2,
+					height: '3rem',
+					duration: 2.5,
 					ease: 'power4.out'
 				},
 				'-=1.0'
@@ -47,13 +51,13 @@
 </script>
 
 <div class="flex h-full w-full flex-col justify-center px-6 md:px-9">
-	<div class="flex flex-col items-start gap-2 md:flex-row md:items-center md:gap-8">
-		<h1 class="text-6xl leading-none font-medium tracking-tighter md:text-9xl">Pedro</h1>
-		<div class="h-12 w-24 bg-accent md:h-24 md:w-48"></div>
-		<h1 class="text-6xl leading-none font-medium tracking-tighter md:text-9xl">Lunet</h1>
+	<div class="flex flex-col items-start gap-4 md:flex-row md:items-center md:gap-8">
+		<h1 class="invisible text-6xl leading-none font-medium tracking-tighter md:text-9xl">Pedro</h1>
+		<div class="invisible h-12 w-24 bg-accent md:h-24 md:w-48"></div>
+		<h1 class="invisible text-6xl leading-none font-medium tracking-tighter md:text-9xl">Lunet</h1>
 	</div>
-	<h2 class="text-2xl font-medium tracking-tight text-text-secondary md:text-7xl">
+	<h2 class="invisible text-2xl font-medium tracking-tight text-text-secondary md:text-7xl">
 		frontend dev
 	</h2>
-	<h3 class="text-lg font-normal text-accent md:text-2xl">porto, portugal</h3>
+	<h3 class="invisible text-lg font-normal text-accent md:mt-2 md:text-2xl">porto, portugal</h3>
 </div>

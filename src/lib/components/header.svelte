4cbac38901fa0300d2c3774a15936921449db2ce
Pedro Lunet <pedrolunet@gmail.com>
2026-02-05 17:23:40 +0000

    improved text size on different breakpoints

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <header
-	class="flex h-(--header-height-mobile) flex-row items-center justify-between px-6 text-base font-semibold tracking-tight md:h-(--header-height-desktop) md:px-9 md:text-xl"
+	class="flex h-(--header-height-mobile) flex-row items-center justify-between px-6 text-base font-semibold tracking-tight md:h-(--header-height-tablet) lg:h-(--header-height-desktop) lg:px-9 lg:text-xl"
 >
 	<a href="/"> pedro lunet </a>
 	<div class="flex flex-row items-center">

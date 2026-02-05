26625041ca81eb6df60371b85fbb92d23415ebfe
Pedro Lunet <pedrolunet@gmail.com>
2026-02-05 12:46:58 +0000

    moved block to a separate component

---
+++ src/lib/components/landing/block.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	let { onclick } = $props();
+</script>
+
+<button
+	{onclick}
+	class="invisible z-50 block cursor-pointer bg-accent transition-opacity hover:opacity-90"
+	class:js-block={true}
+	aria-label="Toggle Navigation"
+></button>

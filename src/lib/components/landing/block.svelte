82b3d6d71b7641d6f8814c935bea791baa79cd57
Pedro Lunet <pedrolunet@gmail.com>
2026-02-06 11:38:30 +0000

    updated block text sizes and removed margins and paddings

--- src/lib/components/landing/block.svelte
+++ src/lib/components/landing/block.svelte
@@ -19,16 +19,16 @@
 	aria-label="Toggle Menu"
 >
 	<div
-		class="relative flex h-full w-full items-end justify-center overflow-hidden bg-accent pb-3 transition-transform duration-200 ease-out active:scale-90 md:pb-4"
+		class="relative flex h-full w-full items-end justify-center overflow-hidden bg-accent transition-transform duration-200 ease-out active:scale-90"
 	>
 		<div
 			class="absolute inset-0 z-0 origin-left scale-x-0 bg-accent-light transition-transform duration-300 ease-out group-hover:scale-x-100"
 		></div>
 
-		<div class="relative z-10 mb-3 flex h-5 w-full justify-center md:mb-4 lg:h-7">
+		<div class="relative z-10 flex h-5 w-full justify-center leading-none lg:h-7">
 			{#key label}
 				<span
-					class="animate-in fade-in zoom-in-95 absolute bottom-0 flex items-end justify-center text-sm font-medium tracking-widest text-bg lowercase duration-300 lg:text-xl"
+					class="animate-in fade-in zoom-in-95 absolute bottom-0 flex items-end justify-center text-lg font-medium tracking-widest text-bg lowercase duration-300 lg:text-3xl"
 				>
 					{label}
 				</span>

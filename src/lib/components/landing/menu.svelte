f6884067167817d183c58322ebd1ead2ee64a918
Pedro Lunet <pedrolunet@gmail.com>
2026-05-26 09:45:57 +0100

    added mural route and adde it to the menu

--- src/lib/components/landing/menu.svelte
+++ src/lib/components/landing/menu.svelte
@@ -39,6 +39,16 @@
 			Contact
 		</span>
 	</a>
+	<a
+		href="/mural"
+		class="menu-item group invisible block text-4xl font-thin tracking-tight text-text md:text-8xl 2xl:text-[clamp(6rem,10vw,10rem)]"
+	>
+		<span
+			class="block origin-right transition-all duration-500 ease-[cubic-bezier(0.25,1,0.5,1)] group-hover:-skew-x-12 group-hover:text-accent"
+		>
+			Mural
+		</span>
+	</a>
 
 	<div class="menu-item invisible my-4 mr-1 h-px w-24 bg-text-secondary 2xl:my-8 2xl:w-40"></div>
 
